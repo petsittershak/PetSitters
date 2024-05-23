01a15da87208bd6790788fc8ad6fa371f0b51a84
4got10king <Aldavletshina77654@yandex.ru>
2024-05-23 14:27:21 +0300

    Исправил стиль

--- migrations/env.py
+++ migrations/env.py
@@ -13,11 +13,11 @@ config = context.config
 
 section = config.config_ini_section
 config.set_section_option(section, "DB_HOST", DB_HOST)
-config.set_section_option(section,'DB_PORT', DB_PORT)
+config.set_section_option(section, "DB_PORT", DB_PORT)
 config.set_section_option(section, "DB_NAME", DB_NAME)
-config.set_section_option(section,"DB_USER", DB_USER)
+config.set_section_option(section, "DB_USER", DB_USER)
 config.set_section_option(section, "DB_PASS", DB_PASS)
-config.set_section_option(section, 'DB', DB)
+config.set_section_option(section, "DB", DB)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -74,9 +74,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
--- src/models/models.py
+++ src/models/models.py
@@ -22,12 +22,14 @@ roles = Table(
     Column("permissions", JSON),
 )
 
-clients = Table('clients', metadata,
-                Column('id', Integer, primary_key=True),
-                Column('name', String, nullable=False),
-                Column("email", String, nullable=False),
-                Column('number', Integer, nullable=False),
-                Column("registered_at", TIMESTAMP, default=datetime.datetime.utcnow()),
-                Column('role_id', Integer, ForeignKey('roles.id')),
-                Column("mailing", Boolean)
-                )
+clients = Table(
+    "clients",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String, nullable=False),
+    Column("email", String, nullable=False),
+    Column("number", Integer, nullable=False),
+    Column("registered_at", TIMESTAMP, default=datetime.datetime.utcnow()),
+    Column("role_id", Integer, ForeignKey("roles.id")),
+    Column("mailing", Boolean),
+)
