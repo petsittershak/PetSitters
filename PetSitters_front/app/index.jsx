a3265d04b90d4fa859bbd805e465d87c3df93c72
MargoG197 <goncharovarita@mail.ru>
2024-06-05 10:56:45 +0300

    added frontend structure, downloaded pictures from figma, created main and petsitters pages with stracture

---
+++ PetSitters_front/app/index.jsx
@@ -0,0 +1,17 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
+import  Header  from "./components/header.jsx";
+
+  
+const header = "Рассказ";
+
+  
+// ReactDOM.createRoot(
+//     document.getElementById("header")
+// )
+// .render(
+//     <div>
+//         <Header text={header} />
+//     </div>
+// );
+
