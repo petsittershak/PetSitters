cbbba95401943c0ecc97496490dcdfb7add3801c
4got10king <Aldavletshina77654@yandex.ru>
2024-06-06 20:59:03 +0300

    DONE

--- app/application/schemas/schemas.py
+++ app/application/schemas/schemas.py
@@ -1,6 +1,6 @@
 from typing import Optional
-
-from pydantic import BaseModel, ConfigDict
+import re
+from pydantic import BaseModel, ConfigDict, validator
 
 
 class SClientAdd(BaseModel):
@@ -8,7 +8,16 @@ class SClientAdd(BaseModel):
     pet: str
     price: int
     city: str
+    telephone: int
     description: Optional[str] = None
+
+    @validator("telephone")
+    def validate_phone_number(cls, v):
+        if not re.match(r"^\d{11}$", str(v)):
+            raise ValueError(
+                "Неверный формат номера телефона. Пожалуйста, используйте формат 89999999999"
+            )
+        return v
 
 
 class SClient(SClientAdd):
