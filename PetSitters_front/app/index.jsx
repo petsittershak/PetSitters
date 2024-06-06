3ec8f4d6204ed5099f7370226c75ddc807e91c43
MargoG197 <goncharovarita@mail.ru>
2024-06-06 19:02:38 +0300

    worked on customer form, added styles, added choice on service, animal and added geoposition automatic choice of the customer city

--- PetSitters_front/app/index.jsx
+++ PetSitters_front/app/index.jsx
@@ -1,17 +1,17 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
 import  Header  from "./components/header.jsx";
-
+import Form from "./components/client_form.jsx"
   
 const header = "Рассказ";
 
   
-// ReactDOM.createRoot(
-//     document.getElementById("header")
-// )
-// .render(
-//     <div>
-//         <Header text={header} />
-//     </div>
-// );
+ReactDOM.createRoot(
+    document.getElementById("form_div")
+)
+.render(
+    <div>
+        <Form />
+    </div>
+);
 
