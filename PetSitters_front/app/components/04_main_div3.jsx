1e11df850cd885527d4e03651ae661ed844b4951
MargoG197 <goncharovarita@mail.ru>
2024-06-08 19:29:01 +0300

    created content for main page div3

--- PetSitters_front/app/components/04_main_div3.jsx
+++ PetSitters_front/app/components/04_main_div3.jsx
@@ -1,34 +1,52 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-
-
-function CreateDiv() {
-  
-
-
+function CreateBlocks({ options, description }) {
   return (
-    <div>
-          <div>
-      <h1>Как работает Petsitter</h1>
+    <div className="returnBlocksDiv">
+      {options.map((opt, i) => {
+        return (
+        <div key={i} className="howPetsittersWork_optionsDivs">
+          <img src={opt} className="gif" />
+          <p className="howPetsittersWorkDescription">{description[i]}</p>
+          </div>
+        )
+      })}
     </div>
-    <div>
-        <div>
-
-        </div>
-        <img src='./app/pictures/pic/06_picture.jpg' alt="Описание" />
-    </div>
-    </div>
-
-
-  )
+  );
 }
 
-
 export default function HowPetsittersWork() {
+  const gifArray = [
+    "./app/pictures/gif/01_gif_request.gif",
+    "./app/pictures/gif/02_Gif_choose_sitter.gif",
+    "./app/pictures/gif/03_gif_payment.gif",
+    "./app/pictures/gif/04_gif_catsitter.gif",
+    "./app/pictures/gif/05_gif_dogsitter.gif",
+    "./app/pictures/gif/06_gif_reports.gif",
+  ];
+
+  const descriptionArr = [
+    "Вы оставляете заявку на передержку на сайте",
+    "Вы выбираете идеального sitter из предложенных нами вариантов",
+    "Оплачиваете передержку Вашей банковской картой",
+    "Dogsitter гуляет с вашим собакеном, моет ему лапы после прогулки, дает корм соглаcно вашим пожеланиям",
+    "Catsitter играет с вашим котиком, кормит и следит за чистотой лотка",
+    "Во время передержки sitter отправляет отчеты, чтобы Вы были уверены - ваш питомец в безопасности и с ним все хорошо",
+  ];
+
   return (
-    
-      <CreateDiv />
-   
-   ) 
-  
+    <div className="main_page_div3">
+      <div>
+        <h1>Как работает Petsitter</h1>
+      </div>
+      <div className="mainPageDiv3_2_block">
+          <CreateBlocks options={gifArray} description={descriptionArr} />
+        <img
+          src="./app/pictures/pic/06_picture.jpg"
+          alt="Описание"
+          className="picture"
+        />
+      </div>
+    </div>
+  );
 }
